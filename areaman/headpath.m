68ca6d6708b7dd0a8f62aa5d881c1132874d7d72
lymslive <403708621@qq.com>
2015-11-28 14:24:30 +0800

    add relative path

--- areaman/headpath.m
+++ areaman/headpath.m
@@ -6,7 +6,7 @@
 %
 % 输入参数：
 % 如果 n 是正数，表示显示前 n 个 path
-% 如果 n 是负数，表示删除第 n 个 path
+% 如果 n 是负数，表示删除第 n 个 path, 然后显示新 path 的前 n+1 项
 % 如果 n 是 0，则运行内置 path 命令
 % 如果省略参数，默认是 +10，显示前10个 path
 %
@@ -14,8 +14,19 @@
 % 返回值 out 是 cellstr 类型, n*1 列向量
 % 注意内置 p = path 返回的是以 ; 分隔的长串字符串
 %
+% 特殊用法：
+% 当传入一个字符串时，将当作相对路径，转化为绝对路径后添加到 path 头部
+% 可用 '.' 与 '..' 表示当前路径与父目录
+% 此时返回新添加的绝对路径
+%
 % maintain: lymslive / 2015-11
 function out = headpath(n)
+
+if nargin >= 1 && ischar(n)
+	out = [pwd, filesep, n];
+	addpath(out);
+	return;
+end
 
 if nargin < 1
 	n = 10;
