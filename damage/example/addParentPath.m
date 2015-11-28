e5f5556275cbfdf8a3985423c305cd851e403549
lymslive <403708621@qq.com>
2015-11-28 10:18:09 +0800

    use .. as parent path

--- damage/example/addParentPath.m
+++ damage/example/addParentPath.m
@@ -1,11 +1,10 @@
 % 获得当前脚本文件名，将父目录添加到 path 中
 % 以便能用上层目录中的函数，测试父目录提供的工具
+% 返回父目录，用 .. 表示的父目录
 	
 function parent = addParentPath()
 
-	filename = mfilename('fullpath');
-	[pathstr, name, ext] = fileparts(filename);
-	parent = regexprep(pathstr, '\w+$', '');
+	parent = [pwd, filesep, '..'];
 	addpath(parent);
 
 end %-of main
