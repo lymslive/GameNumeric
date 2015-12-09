65b818f5b1fcbf66707a0190890f2d370c57633f
lymslive <403708621@qq.com>
2015-12-09 19:17:37 +0800

    remove trival 0

--- functions/refine.m
+++ functions/refine.m
@@ -1,25 +1,25 @@
 % refine: refine a curve
-% 细化曲线
-% 根据一个较粗的曲线表，n*2 数组
-% 求出一个细化的曲线表，m*2 数组
+% ϸ������
+% ����һ���ϴֵ����߱���n*2 ����
+% ���һ��ϸ�������߱���m*2 ����
 %
-% 输入参数：
-% @short: 两列，第2列是第1列的函数，但只有一些离散的点
-%  最好第1列要求单调升序，第2列不能有重复的值
-%  每行的两个值相当于组成一个数据点
+% ���������
+% @short: ���У���2���ǵ�1�еĺ�������ֻ��һЩ��ɢ�ĵ�
+%  ��õ�1��Ҫ�󵥵����򣬵�2�в������ظ���ֵ
+%  ÿ�е�����ֵ�൱�����һ�����ݵ�
 %
-% @option: 额外选项，可选参数
-%  .method: 插值方法，同interp1 的 method 参数，默认是 'linear'
-%  .addorigin: 是否在 short 首行添加零值 [0, 0]，默认 false
-%  .plot: 是否绘图查看插值曲线结果
+% @option: ����ѡ���ѡ����
+%  .method: ��ֵ������ͬinterp1 �� method ������Ĭ���� 'linear'
+%  .addorigin: �Ƿ��� short ����������ֵ [0, 0]��Ĭ�� false
+%  .plot: �Ƿ��ͼ�鿴��ֵ���߽��
 %
-% 输出参数：
-% @long: 两列，第1列是细化的自变量，范围从 short 的第1行到最后一行
-%  步长默认为1，游戏中涉及的曲线的自变量一般是自然数
-%  在 short 数组中未指定的中间点，用插值的方式求出
+% ���������
+% @long: ���У���1����ϸ�����Ա�������Χ�� short �ĵ�1�е����һ��
+%  ����Ĭ��Ϊ1����Ϸ���漰�����ߵ��Ա���һ������Ȼ��
+%  �� short ������δָ�����м�㣬�ò�ֵ�ķ�ʽ���
 %
-% 其他说明：
-% 算法主要调用 interp1 插值函数
+% ����˵����
+% �㷨��Ҫ���� interp1 ��ֵ����
 %
 % maintain: lymslive / 2015-11
 function long = refine(short, option)
@@ -55,6 +55,10 @@ long = [x, y];
 
 if isfield(option, 'plot') && option.plot
 	plot(x, y, 'b-', short(:, 1), short(:, 2), 'ro');
+end
+
+if isfield(option, 'addorigin') && option.addorigin
+        long(1,:) = [];
 end
 
 end %-of main
