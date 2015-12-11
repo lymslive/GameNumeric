136d2876a43cd5b4506e8b178d528386a1cf6417
lymslive <403708621@qq.com>
2015-12-11 23:18:57 +0800

    pause in test

--- extra/@cList/main_.m
+++ extra/@cList/main_.m
@@ -1,12 +1,20 @@
-function tf = main_(varargin)
+function tf = main_(reply, varargin)
 % a test driver methods
 
-s_Test();
+if nargin < 1 || isempty(reply)
+    reply = 'n';
+elseif ischar(reply) && lower(reply(1)) == 'y'
+    reply = 'y';
+else
+    reply = 'n';
+end
 
-tf = true;
+tf = s_Test(reply);
+
 end %F main
 
-function s_Test()
+function tf = s_Test(reply)
+tf = false;
 
 fprintf('creat an empty stack:\n');
 stack = cList();
@@ -18,6 +26,8 @@ for i = 1 : 5
 end
 assert(stack.count == 5, 'properties count fails');
 
+reply = s_pause(reply);
+
 fprintf('test top() method:\n');
 assert(stack.top() == 5, 'method top() fails');
 assert(stack.top(3) == 3, 'method top(i) fails');
@@ -27,6 +37,8 @@ oldstack = stack;
 copystack = stack.clone();
 fprintf('display the objects:\n');
 oldstack,
+reply = s_pause(reply);
+
 fprintf('test pop() method:\n');
 assert(stack.top() == stack.pop(), 'pop ~= top ??');
 assert(stack.count == 4, 'pop donnot remove element?');
@@ -35,9 +47,11 @@ assert(oldstack.top() == stack.top(), 'handle class fails?');
 assert(stack.pop(3) == 3, 'pop(i) fails?');
 assert(stack.pop(1) == 1, 'pop(1) fails?');
 stack,
+reply = s_pause(reply);
 
 fprintf('see cloned stack\n');
 copystack,
+reply = s_pause(reply);
 
 fprintf('retrive the cloned stack\n');
 stack = copystack;
@@ -49,6 +63,7 @@ vector = stack.cat();
 assert(all(vector == [1:5]'), 'cat() fails?');
 disp(datatype);
 disp(vector);
+reply = s_pause(reply);
 
 fprintf('test up and down method\n');
 fprintf('up():\n');
@@ -63,13 +78,16 @@ fprintf('up(2, true):\n');
 stack.up(2, true),
 fprintf('down(5, true):\n');
 stack.down(5, true),
+reply = s_pause(reply);
 
 fprintf('see the oldstack\n');
 oldstack,
+reply = s_pause(reply);
 
 fprintf('construct from varargin:\n');
 list = cList('abc', 'bcd', 'cde', 'def');
 list,
+reply = s_pause(reply);
 fprintf('test iterator():\n');
 next = list.iterator();
 disp(next());
@@ -77,6 +95,8 @@ disp(next());
 disp(next());
 disp(next());
 assert(isempty(next()), 'next() fails');
+reply = s_pause(reply);
+
 fprintf('test iterator(-1):\n');
 prev = list.iterator(-1);
 disp(prev());
@@ -84,6 +104,7 @@ disp(prev());
 disp(prev());
 disp(prev());
 assert(isempty(prev()), 'prev() fails');
+reply = s_pause(reply);
 
 fprintf('use iterator in while loop\n');
 next = list.iterator();
@@ -92,5 +113,33 @@ while val
     disp(val);
     val = next();
 end
+reply = s_pause(reply);
 
+disp('test ended');
+tf = true;
+
+end %F sub
+
+% pause the test, let user see the above test result, and
+% wait user enter a replay: 
+% -y: no pause further more
+% -q: quit the test
+% -n: next test, default
+function reply = s_pause(reply)
+
+if reply == 'y'
+    return;
+end
+
+reply = input('<<Enter to continue or y to silence continue? :', 's');
+
+if isempty(reply)
+    reply = 'n';
+else
+    if lower(reply(1)) ~= 'y'
+        reply = 'n';
+    else
+        reply = 'y';
+    end
+end
 end %F sub
